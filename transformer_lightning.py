7fc2f349228dc0eda8874c78c86b1e68ee4bab99
chengxiao19961022 <769478099@qq.com>
2020-10-09 08:46:45 +0000

    [add] transformer pytorch

--- transformer_lightning.py
+++ transformer_lightning.py
@@ -9,7 +9,7 @@ from typing import Dict, Tuple, List
 import time
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, Transformer
 
 
 class TextBatch:
@@ -152,9 +152,15 @@ class TransformerModelLightning(pl.LightningModule):
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         self.encoder = nn.Embedding(self.ntoken, ninp)
         self.ninp = ninp
-        self.decoder = nn.Linear(ninp, self.ntoken)
+        self.transformer = Transformer(d_model=ninp,
+                                       nhead=nhead,
+                                       num_encoder_layers=nlayers,
+                                       num_decoder_layers=1,
+                                       dim_feedforward=nhid,
+                                       dropout=dropout)
+        self.out = nn.Linear(ninp, self.ntoken)
 
-        # self.init_weights()
+        self.init_weights()
 
     def _generate_square_subsequent_mask(self, sz):
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
@@ -165,8 +171,8 @@ class TransformerModelLightning(pl.LightningModule):
     def init_weights(self):
         initrange = 0.1
         self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-initrange, initrange)
+        self.out.bias.data.zero_()
+        self.out.weight.data.uniform_(-initrange, initrange)
 
     # ===== OPTIMIZERS =====
     def configure_optimizers(self):
@@ -259,7 +265,7 @@ class TransformerModelLightning(pl.LightningModule):
         src = self.encoder(src) * math.sqrt(self.ninp)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src, self.src_mask)
-        output = self.decoder(output)
+        output = self.out(output)
         return output
 
     def transfer_batch_to_device(self, batch: TextBatch,
