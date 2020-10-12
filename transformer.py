ca1893e300cf0a22c6ccdf242a0a6bc7db621ba0
chengxiao19961022 <769478099@qq.com>
2020-10-12 10:39:26 +0000

    [add] cook, [update] evaluate

--- transformer.py
+++ transformer.py
@@ -44,6 +44,27 @@ def train():
     trainer.fit(model)
 
 
+def evaluate(checkpoint: str):
+    TEXT = torchtext.data.Field(tokenize=get_tokenizer("basic_english"),
+                                init_token='<sos>',
+                                eos_token='<eos>',
+                                lower=True)
+    train_txt, val_txt, test_txt = torchtext.datasets.WikiText2.splits(TEXT)
+    TEXT.build_vocab(train_txt)
+    model = TransformerModelLightning.load_from_checkpoint(
+        checkpoint_path=checkpoint)
+    data = TEXT.numericalize([test_txt.examples[0].text])
+    seq_num = data.size(0) // 35
+    data = data.narrow(0, 0, seq_num * 35 + 1)
+    dataloader = DataLoader(TextIterDataset(data, 35, True),
+                            batch_size=10,
+                            collate_fn=TextBatch.collate_wrapper,
+                            pin_memory=True)
+    gpu = 1 if torch.cuda.is_available() else None
+    trainer = pl.Trainer(gpus=gpu)
+    trainer.test(model, test_dataloaders=[dataloader])
+
+
 if __name__ == "__main__":
-    train()
-    # evaluate("lightning_logs/version_0/checkpoints/epoch=2.ckpt")
+    # train()
+    evaluate("ts_logger/transformer/version_1/epoch=08-val_loss=8.1653.ckpt")
